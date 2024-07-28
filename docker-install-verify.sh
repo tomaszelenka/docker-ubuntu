ca435a1b838f7726f582abdcd0fb8f270bf1e795
tomaszelenka <30825620+tomaszelenka@users.noreply.github.com>
2024-07-28 22:00:03 +0200

    Update docker-install-verify.sh

--- docker-install-verify.sh
+++ docker-install-verify.sh
@@ -17,5 +17,5 @@ sudo apt-get update
 #Install the Docker packages 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
  
-#Verify that the Docker Engine
+#Verify the Docker Engine
 sudo docker run hello-world
